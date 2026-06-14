f658ee05bba54e7c2b026995f00f2f9f058751ce
Endrio Agassi <62084856+Endrio1@users.noreply.github.com>
2026-06-14 15:53:11 -0300

    SCRUM-4 : Adiciona testes para inserções inválidas

--- tests/conftest.py
+++ tests/conftest.py
@@ -13,7 +13,7 @@ from main import criar_app, items
 @pytest.fixture
 def client():
     app = criar_app()
-    app.config['TESTING'] = True
+    app.config["TESTING"] = True
     with app.test_client() as client:
         with app.app_context():
             items.clear()
--- tests/test_main.py
+++ tests/test_main.py
@@ -4,6 +4,7 @@ from main import app, items
 
 client = app.test_client()
 
+
 def setup_function():
     items.clear()
 
@@ -28,6 +29,22 @@ def test_mostrar_items():
     assert resp.status_code == 200
     js = resp.get_json()
     assert len(js["items"]) == 2
+
+
+def test_cria_item_missing_nome():
+    data = {"descricao": "Desc", "preco": 10.5}
+    resp = client.post("/items/", json=data)
+    assert resp.status_code == 400
+    js = resp.get_json()
+    assert js.get("detail") == "Campos obrigatórios ausentes"
+
+
+def test_cria_item_missing_preco():
+    data = {"nome": "Produto B", "descricao": "Desc"}
+    resp = client.post("/items/", json=data)
+    assert resp.status_code == 400
+    js = resp.get_json()
+    assert js.get("detail") == "Campos obrigatórios ausentes"
 
 
 def test_buscar_item_not_found():
